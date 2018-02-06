b4d17c1d1f7af7d42e9a2fb96074e8f100bda5c1
Alice Darner <alice.darner@gmail.com>
2018-02-06 10:34:35 +0100

    init dbs

--- Pagina/Pagina/ViewController.swift
+++ Pagina/Pagina/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func login(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
