0083cf7b8435e45fa759f43602c2cdc30a2dbbfd
Jessika Tibblin <jessikatibblin@hotmail.com>
2018-02-08 04:42:25 -0800

    added viewcontrollers for tableview and viewcontroller for story editing

---
+++ Pagina/Pagina/ChapterTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChapterTableViewCell.swift
+//  Pagina
+//
+//  Created by user on 2/8/18.
+//  Copyright © 2018 dogbird. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ChapterTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var ChapterTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Pagina/Pagina/ChapterTableViewController.swift
+++ Pagina/Pagina/ChapterTableViewController.swift
@@ -1,63 +1,24 @@
 //
-//  StoryTableViewController.swift
+//  ChapterTableViewController.swift
 //  Pagina
 //
-//  Created by Alice Darner on 2018-02-05.
+//  Created by user on 2/8/18.
 //  Copyright © 2018 dogbird. All rights reserved.
 //
 
 import UIKit
 import Firebase
 
-class StoryTableViewController: UITableViewController {
+class ChapterTableViewController: UITableViewController {
 
-    var ref: DatabaseReference!
-    @IBOutlet var storyTableView: UITableView!
-    
-    struct Story{
-        var id = "";
-        var title = "No title";
-    }
-    
-    var userid = "user"; //To be used if auth is implemented
-    
-    var stories:[Story] = [];
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Firebase dbs reference
-        ref = Database.database().reference()
-        //so we can use hardcoded books
-//        if Auth.auth().currentUser != nil {
-//            print(Auth.auth().currentUser?.email)
-//            userid = (Auth.auth().currentUser?.uid)!;
-//        } else {
-//            // No user is signed in.
-//            // ...
-//        }
-        
-        //getStories(storyArray:&stories, ref: ref, user: userid);
-        ref.child("users").child(userid).child("stories").observeSingleEvent(of: DataEventType.value, with: { (snapshot) in
-            for child in snapshot.children.allObjects as! [DataSnapshot]{
-                let value = child.value as? NSDictionary;
-                var story = Story();
-                story.title = value?["name"] as? String ?? "";
-                story.id = child.key;
-                self.stories.append(story);
-                self.storyTableView.reloadData();
-            }
-        }) { (error) in
-            print(error.localizedDescription)
-        }
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-    
-    func getStories(storyArray:inout [Story], ref: DatabaseReference, user:String){
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,22 +27,34 @@ class StoryTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return stories.count
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
     }
 
-    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "storyCell", for: indexPath) as! StoryTableViewCell;
-        cell.titleLabel?.text = stories[indexPath.row].title;
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
         return cell
     }
-    
+    */
+
+    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
+    */
 
     /*
     // Override to support editing the table view.
@@ -95,12 +68,12 @@ class StoryTableViewController: UITableViewController {
     }
     */
 
-    
+    /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
- 
+    */
 
     /*
     // Override to support conditional rearranging of the table view.
@@ -110,10 +83,7 @@ class StoryTableViewController: UITableViewController {
     }
     */
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "chapterTableSegue", sender: indexPath.row)
-    }
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -121,6 +91,6 @@ class StoryTableViewController: UITableViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    
+    */
 
 }
