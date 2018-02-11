75bccc8ac3f41c06c4b90d35a2c16e447e9027d4
Jessika Tibblin <jessikatibblin@hotmail.com>
2018-02-11 03:33:02 -0800

    collectionView test

--- Pagina/Pagina/StoryEditViewController.swift
+++ Pagina/Pagina/StoryEditViewController.swift
@@ -1,5 +1,5 @@
 //
-//  StoryEditViewController.swift
+//  wController.swift
 //  Pagina
 //
 //  Created by user on 2/8/18.
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class StoryEditViewController: UIViewController {
+class StoryEditViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+    
+       var items = ["1","2","3","4","5","6","7","8","9"]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! inspirationsCollectionViewCell
+        
+        cell.myLabel.text = items[indexPath.item]
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+    
 
     var currentChapter:ChapterTableViewController.Chapter = ChapterTableViewController.Chapter();
     
@@ -16,7 +38,7 @@ class StoryEditViewController: UIViewController {
     
     @IBOutlet weak var storyEditTextView: UITextView!
     
-    @IBOutlet weak var inspireScrollView: UIScrollView!
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Pagina/Pagina/inspirationsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  inspirationsCollectionViewCell.swift
+//  Pagina
+//
+//  Created by user on 2/11/18.
+//  Copyright © 2018 dogbird. All rights reserved.
+//
+
+import UIKit
+
+class inspirationsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
