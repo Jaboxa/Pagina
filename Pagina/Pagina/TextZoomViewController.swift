30fe16aaaaebb659b6187d6abff13442f438cfbc
Jessika Tibblin <jessikatibblin@hotmail.com>
2018-02-15 10:31:24 +0100

    delete on maps

--- Pagina/Pagina/TextZoomViewController.swift
+++ Pagina/Pagina/TextZoomViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 class TextZoomViewController: UIViewController {
 
     @IBOutlet weak var inspirationTextView: UITextView!
    
+    @IBAction func deleteTextButton(_ sender: Any) {
+        let ref:DatabaseReference = Database.database().reference()
+        
+        if let user = Auth.auth().currentUser {
+    ref.child("users").child(user.uid).child("stories").child(self.currentChapter.storyid).child("chapters").child(self.currentChapter.id).child("inspirations").child(self.currentInspiration.id).removeValue();
+        navigationController?.popViewController(animated: true);
+        }
+        
+        
+    }
     var currentInspiration: StoryEditViewController.Inspiration = StoryEditViewController.Inspiration();
     var currentChapter: ChapterTableViewController.Chapter = ChapterTableViewController.Chapter();
     
